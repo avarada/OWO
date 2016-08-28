3efc9f7bfc6be5007a58c15a9414d8e7408d9938
anari sengbe <anari@productank.com>
2016-08-27 20:46:29 -0700

    login implemented

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -14,9 +14,12 @@ export class HomePage {
 
     http:any
     message:any
-    player:any
+    name:any
     user:any
     device_id:any
+    passcode:any
+    uid:any
+    form:any
 
   constructor(private nav: NavController, navParams : NavParams,private platform: Platform, http: Http) {
 
@@ -24,8 +27,53 @@ export class HomePage {
     this.nav = nav
     this.user = {}
     this.device_id = Device.device.uuid
+    this.form ={}
+    this.passcode = ''
+
 
   }
+
+  add(value){
+    if(this.passcode.length<4){
+       this.passcode = this.passcode + value;
+    }
+  };
+
+  delete(){
+
+    if(this.passcode.length > 0) {
+       this.passcode = this.passcode.substring(0, this.passcode.length - 1);
+   }
+ };
+
+ login(){
+
+   let link = 'https://gamerholic.com/server/owo/session.php';
+   var data = JSON.stringify({device_id: this.device_id,code:this.passcode});
+   let headers = new Headers({ 'Content-Type': 'application/json' });
+   let options = new RequestOptions({ headers: headers });
+
+   return this.http.post(link, data,options)
+   //print php error
+   //.subscribe(data => {console.log(data._body)})
+
+    //get json response
+     .map(data => data.json())
+     .catch(this.handleError)
+     .subscribe((data) =>{
+       this.uid = data.uid,
+       this.name = data.name,
+       this.message = data.message,
+       setTimeout(()=>{
+
+         this.get_user()
+
+       },1000)
+     }
+ )
+
+
+ }
 
   loginFB(type){
     this.platform.ready().then(() => {
@@ -39,15 +87,15 @@ export class HomePage {
 
           return this.http.post(link, pass,options)
           //print php error
-          //.subscribe(data => {console.log(data._body)})
+          .subscribe(data => {console.log(data._body)})
 
            //get json response
-            .map(data => data.json())
-            .catch(this.handleError)
-            .subscribe((data) => this.user = data,
-               setTimeout(()=>{
-                 //this.getUser()
-               },1200))
+            // .map(data => data.json())
+            // .catch(this.handleError)
+            // .subscribe((data) => this.user = data,
+            //    setTimeout(()=>{
+            //      //this.getUser()
+            //    },1200))
 
         }else{
           alert(JSON.stringify(result))
@@ -56,6 +104,11 @@ export class HomePage {
        })
     })
   }
+
+  get_user(){
+
+  }
+
 
   handleError(error){
         console.error(error);
